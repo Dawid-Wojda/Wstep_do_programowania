f03f8e09039208be9893effbe4a5979c884c4fc1
Dawid Wojda <165642@student.uwm.edu.pl>
2021-11-06 21:48:27 +0100

    Add new files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Dawid Wojda" << endl;
+    cout << "Dawid" << endl << "Wojda" << endl;
+    cout << "Dawid" << " ";
+    cout << "Wojda" << endl;
+    return 0;
+}
